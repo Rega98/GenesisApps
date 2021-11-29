238e4774e8a8b894f5c7b7e33564622fa4ee610e
Ismael SL <ucmex_isma@hotmail.com>
2021-11-28 20:21:04 -0600

    -- Agregando conf para reconocer servlet
    Se necesita agregar el servlet dentro del web.xml para que las rutas puedan funcionar.

--- GenesisApp/src/java/Controller/PagoController.java
+++ GenesisApp/src/java/Controller/PagoController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package Controller;
 
 import Model.Pago;
@@ -15,7 +11,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
